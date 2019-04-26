5a4e1d78ccb795b06e888bea0fd1e7d3b731f4d1
smartwalle <smartwalle@gmail.com>
2019-04-26 09:17:31 +0800

    update

--- user/user.go
+++ user/user.go
@@ -5,6 +5,6 @@ import (
 )
 
 var (
-	UsernameExists = errors.New("110001", "用户名已经存在")
-	UserNotExist   = errors.New("110002", "用户信息不存在")
+	UsernameExists = errors.New(110001, "用户名已经存在")
+	UserNotExist   = errors.New(110002, "用户信息不存在")
 )
