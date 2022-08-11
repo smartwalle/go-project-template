d80284b4e5f9b97f20ec9ae700f2ef053fe845b6
smartwalle <smartwalle@gmail.com>
2022-08-11 11:37:03 +0800

    结构调整

---
+++ user/transport/grpc/user_handler.go
@@ -0,0 +1,31 @@
+package grpc
+
+import (
+	"context"
+	userGrpc "go-project-template/user/api/grpc"
+	"go-project-template/user/service"
+	"google.golang.org/grpc"
+)
+
+type UserHandler struct {
+	userGrpc.UnimplementedUserServer
+	userService *service.UserService
+}
+
+func NewUserHandler(userService *service.UserService) *UserHandler {
+	var h = &UserHandler{}
+	h.userService = userService
+	return h
+}
+
+func (this *UserHandler) Handle(s *grpc.Server) {
+	userGrpc.RegisterUserServer(s, this)
+}
+
+func (this *UserHandler) GetUserWithId(ctx context.Context, req *userGrpc.GetUserReq) (*userGrpc.GetUserRsp, error) {
+	user, err := this.userService.GetUserWithId(req.Id)
+	if err != nil {
+		return nil, err
+	}
+	return NewGetUserRsp(user), nil
+}
